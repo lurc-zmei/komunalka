c8dd4d36b44cf953d96de97854ff5f1283506862
lurc-zmei <lurc-zmei@inbox.ru>
2023-09-26 20:06:50 +0300

    начальная структура проекта

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/connect.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Komunalka2</title>
+</head>
+<body>
+	<h2>Версия 2</h2>
