e3876c573322d5a4fd05fcdad7e0bd944684213b
lurc-zmei <lurc-zmei@inbox.ru>
2023-09-28 19:46:19 +0300

    Исключили из репозитория данные для подключения БД

--- connect.php
+++ connect.php
@@ -1,2 +1,8 @@
 <?php
-$sqlConnect = mysqli_connect('localhost', 'root', '', 'komunalka');
+
+$sqlConnect = mysqli_connect(
+	$config['DB']['SERVER'], 
+	$config['DB']['USER'], 
+	$config['DB']['PASSWORD'], 
+	$config['DB']['NAME']
+);
--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
+# Файл настроек
+$config = require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+
 # Подключение к БД
-include($_SERVER['DOCUMENT_ROOT'].'/connect.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/connect.php');
 
 # Анализируем и разбиваем адрес
 $parsed_url = parse_url($_SERVER['REQUEST_URI']);
 $arSection = array_values(array_filter(explode('/', $parsed_url['path'])));
 
 # Подключаем шаблон страницы с базовой структурой
-include($_SERVER['DOCUMENT_ROOT'].'/templates/design.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/design.php');
